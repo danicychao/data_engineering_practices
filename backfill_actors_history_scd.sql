9b2a0809592460d97f45c39fb013a916b318c957
danichao <danichao@gapp.nthu.edu.tw>
2025-08-19 19:38:52 +0200

    actors_history_scd incremental query

---
+++ backfill_actors_history_scd.sql
@@ -0,0 +1,45 @@
+INSERT INTO actors_history_scd
+WITH previous AS (
+SELECT
+actor,
+actorid,
+quality_class,
+LAG(quality_class) OVER (PARTITION BY actorid ORDER BY current_year) as last_year_class,
+is_active,
+LAG(is_active) OVER (PARTITION BY actorid ORDER BY current_year) as last_year_active,
+current_year
+FROM actors
+),
+
+change_indicator AS (
+SELECT
+actor,
+actorid,
+quality_class,
+is_active,
+CASE WHEN quality_class <> last_year_class THEN 1
+     WHEN is_active <> last_year_active THEN 1
+	 ELSE 0
+	 END as change,
+current_year
+FROM previous
+),
+
+streak_indicator AS (
+SELECT
+*,
+SUM(change) OVER (PARTITION BY actorid ORDER BY current_year) as change_streak
+FROM change_indicator
+)
+
+
+SELECT
+MAX(actor),
+actorid,
+MAX(quality_class),
+is_active,
+MIN(current_year) as start_date,
+MAX(current_year) as end_date,
+2020 as current_year
+FROM streak_indicator
+GROUP BY actorid, change_streak, is_active
