dba4bd0a619bd61a97fc13bddc47278e7531dde0
danichao <danichao@gapp.nthu.edu.tw>
2025-08-19 19:36:14 +0200

    DDL for actors_history_scd table

---
+++ ddl_actors_history_scd.sql
@@ -0,0 +1,10 @@
+CREATE TABLE actors_history_scd (
+	actor TEXT,
+	actorid TEXT,
+	quality_class quality_class,
+	is_active BOOLEAN,
+	start_date INTEGER,
+	end_date INTEGER,
+	current_year INTEGER,
+	PRIMARY KEY (actorid, start_date)
+);
