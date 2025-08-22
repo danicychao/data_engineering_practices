ca983d23b557008d9226ae214d5d8d98afbd2e55
danichao <danichao@gapp.nthu.edu.tw>
2025-08-22 14:22:43 +0200

    re-indent for readability

--- ddl_host_activity_reduced.sql
+++ ddl_host_activity_reduced.sql
@@ -1,7 +1,7 @@
- CREATE TABLE host_activity_reduced (
- host TEXT,
- month_start DATE,
- metric_name TEXT,
- metric_array INTEGER[],
- PRIMARY KEY (host, month_start, metric_name)
- )
+CREATE TABLE host_activity_reduced (
+    host TEXT,
+    month_start DATE,
+    metric_name TEXT,
+    metric_array INTEGER[],
+    PRIMARY KEY (host, month_start, metric_name)
+);
