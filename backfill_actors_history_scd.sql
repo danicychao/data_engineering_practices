74bdb1831e8f88e347dcbf06100946d0ccd9097f
danichao <danichao@gapp.nthu.edu.tw>
2025-08-22 11:11:41 +0200

    reformatting for readability

--- backfill_actors_history_scd.sql
+++ backfill_actors_history_scd.sql
@@ -1,45 +1,42 @@
 INSERT INTO actors_history_scd
 WITH previous AS (
-SELECT
-actor,
-actorid,
-quality_class,
-LAG(quality_class) OVER (PARTITION BY actorid ORDER BY current_year) as last_year_class,
-is_active,
-LAG(is_active) OVER (PARTITION BY actorid ORDER BY current_year) as last_year_active,
-current_year
-FROM actors
+  SELECT
+    actor,
+    actorid,
+    quality_class,
+    LAG(quality_class) OVER (PARTITION BY actorid ORDER BY current_year) AS last_year_class,
+    is_active,
+    LAG(is_active) OVER (PARTITION BY actorid ORDER BY current_year) AS last_year_active,
+    current_year
+  FROM actors
 ),
-
 change_indicator AS (
-SELECT
-actor,
-actorid,
-quality_class,
-is_active,
-CASE WHEN quality_class <> last_year_class THEN 1
-     WHEN is_active <> last_year_active THEN 1
+  SELECT
+    actor,
+    actorid,
+    quality_class,
+    is_active,
+    CASE
+      WHEN quality_class <> last_year_class THEN 1
+      WHEN is_active <> last_year_active THEN 1
 	 ELSE 0
-	 END as change,
-current_year
-FROM previous
+	 END AS change,
+    current_year
+  FROM previous
 ),
-
 streak_indicator AS (
-SELECT
-*,
-SUM(change) OVER (PARTITION BY actorid ORDER BY current_year) as change_streak
-FROM change_indicator
+  SELECT
+    *,
+    SUM(change) OVER (PARTITION BY actorid ORDER BY current_year) AS change_streak
+  FROM change_indicator
 )
-
-
 SELECT
-MAX(actor),
-actorid,
-MAX(quality_class),
-is_active,
-MIN(current_year) as start_date,
-MAX(current_year) as end_date,
-2020 as current_year
+  MAX(actor),
+  actorid,
+  MAX(quality_class),
+  is_active,
+  MIN(current_year) AS start_date,
+  MAX(current_year) AS end_date,
+  2020 AS current_year
 FROM streak_indicator
 GROUP BY actorid, change_streak, is_active
