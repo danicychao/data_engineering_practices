2ab9b6d2a860a8b6c354b47452625b8cf9a0f4ab
danichao <danichao@gapp.nthu.edu.tw>
2025-08-26 16:19:28 +0200

    add useful comments

--- ddl_user_devices_cumulated.sql
+++ ddl_user_devices_cumulated.sql
@@ -1,6 +1,13 @@
+ /*
+  * user_devices_cumulated Table DDL
+  *
+  * Purpose:
+  *   Create table that tracks user active days by browser_type
+  */
+ 
  CREATE TABLE user_devices_cumulated (
     user_id NUMERIC,
- 	device_activity_datelist JSONB,
+ 	device_activity_datelist JSONB, -- {browser type: active dates}
  	PRIMARY KEY (user_id)
  );
 
