3fc114a20cf6ae290f8c9fda8cf67df7cc63c858
danichao <danichao@gapp.nthu.edu.tw>
2025-08-26 06:32:23 +0200

    add useful comments

--- ddl_actors_table.sql
+++ ddl_actors_table.sql
@@ -1,10 +1,25 @@
+/*
+ * Actors Table DDL
+ *
+ * Purpose:
+ *   Create a table, actors, including actors' names, 
+ *   their films and corresponding votes,
+ *   their performance quality, and their active status.
+ * 
+ * Tables:
+ *   - actors
+ * 
+ * Types:
+ *   - film_stat: Composite type for film metrics
+ *   - quality_class: Enumeration for actor performance quality
+ */
+
 CREATE TYPE film_stat AS (
  	film TEXT,
  	votes INTEGER,
  	rating REAL,
  	filmid TEXT
  );
-
 
 CREATE TYPE quality_class AS
   	ENUM ('star', 'good', 'average', 'bad');
@@ -14,7 +29,7 @@ CREATE TABLE actors (
  	actorid TEXT,
  	films film_stat[],
  	quality_class quality_class,
- 	is_active BOOLEAN,
+ 	is_active BOOLEAN, -- whether actor is making films that year
  	current_year INTEGER,
  	PRIMARY KEY (actorid, current_year)
  );
