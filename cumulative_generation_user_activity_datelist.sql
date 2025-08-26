a50cc4e145d8238c225cc50c03f263d557a2c68e
danichao <danichao@gapp.nthu.edu.tw>
2025-08-26 18:37:51 +0200

    add useful comments

--- cumulative_generation_user_activity_datelist.sql
+++ cumulative_generation_user_activity_datelist.sql
@@ -1,4 +1,18 @@
+/*
+ * Cumulative query for device_activity_datelist table
+ *
+ * Purpose:
+ *   Generate device_activity_datelist table
+ *   from events table
+ * 
+ * Tables:
+ *   - device_activity_datelist (target table)
+ *   - events table (source table)
+ */
+
 INSERT INTO user_devices_cumulated
+
+-- Deduplicate the events table
 WITH user_row AS (
     SELECT
         user_id,
@@ -7,12 +21,13 @@ WITH user_row AS (
         ROW_NUMBER() OVER (
             PARTITION BY user_id, device_id, event_time 
             ORDER BY event_time
-        ) as row_num
+        ) as row_num -- for deduplication
     FROM events
     WHERE user_id IS NOT NULL
     AND device_id IS NOT NULL
 ),
 
+-- Select user's browser type from the devices table
 user_device_browser AS (
     SELECT
         u.user_id as user_id,
@@ -21,9 +36,10 @@ user_device_browser AS (
         DATE(u.event_time) as act_date
     FROM user_row u 
     JOIN devices d ON u.device_id = d.device_id
-    WHERE u.row_num = 1
+    WHERE u.row_num = 1 -- select only first row to avoid duplication
 ),
 
+-- Deduplicate user-browser-active date
 user_device_browser_dedup AS (
     SELECT
         *,
@@ -34,6 +50,7 @@ user_device_browser_dedup AS (
     FROM user_device_browser
 ),
 
+-- Aggregate active dates by browser type
 user_id_browser AS (
     SELECT
         user_id,
@@ -44,6 +61,8 @@ user_id_browser AS (
     GROUP BY 1, 2
 )
 
+-- Aggregate multiple browser-active dates columns into one by user id
+-- and insert into the user_devices_cumulated table
 SELECT
     user_id as user_id,
     JSONB_OBJECT_AGG(browser_type, dates) as device_activity_datelist
