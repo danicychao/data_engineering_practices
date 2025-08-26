e8aed476c10b4fc629456e5421d87408c01fcb8d
danichao <danichao@gapp.nthu.edu.tw>
2025-08-26 08:30:52 +0200

    add useful comments

--- ddl_actors_history_scd.sql
+++ ddl_actors_history_scd.sql
@@ -1,3 +1,18 @@
+/*
+ * actors_history_scd Table DDL
+ *
+ * Purpose:
+ *   Create a table, actors_history_scd, 
+ *   tracking actors' performance quality, and their active status.
+ * 
+ * Tables:
+ *   - actors_history_scd
+ * 
+ *
+ * Data will be inserted into actors_history_scd table 
+ * by backfill_actors_history_scd.sql.
+ */
+
 CREATE TABLE actors_history_scd (
 	actor TEXT,
 	actorid TEXT,
