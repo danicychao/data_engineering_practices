e635e2ab4f3c5a6641045039173b090fc640cc9a
danichao <danichao@gapp.nthu.edu.tw>
2025-08-27 05:01:30 +0200

    add useful comments

--- ddl_hosts_cumulated.sql
+++ ddl_hosts_cumulated.sql
@@ -1,6 +1,20 @@
+/*
+ * hosts_cumulated Table DDL
+ *
+ * Purpose:
+ *   Create a table, hosts_cumulated, 
+ *   tracking dates host experiencing any activity
+ * 
+ * Tables:
+ *   - hosts_cumulated
+ *
+ * Data will be inserted into hosts_cumulated table by 
+ * host_activity_datelist_incremental_generation_query.sql.
+ */
+
 CREATE TABLE hosts_cumulated (
     host TEXT,
-    host_activity_datelist DATE[],
+    host_activity_datelist DATE[], -- dates host experiencing activity
     cur_date DATE,
     PRIMARY KEY (host, cur_date)
 );
