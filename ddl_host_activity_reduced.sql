c5f71735b075a87627b405aa94e31351ba37bd4d
danichao <danichao@gapp.nthu.edu.tw>
2025-08-27 08:29:56 +0200

    add useful comments

--- ddl_host_activity_reduced.sql
+++ ddl_host_activity_reduced.sql
@@ -1,3 +1,18 @@
+/*
+ * host_activity_reduced Table DDL
+ *
+ * Purpose:
+ *   Create a reduced fact table, host_activity_reduced, 
+ *   including conclusion metrics such as
+ *   number of hits and number of unique visitors.
+ * 
+ * Tables:
+ *   - host_activity_reduced
+ *
+ * Data will be loaded into host_activity_reduced table by 
+ * incremental_host_activity_reduced_[METRIC NAME].sql.
+ */
+
 CREATE TABLE host_activity_reduced (
     host TEXT,
     month_start DATE,
