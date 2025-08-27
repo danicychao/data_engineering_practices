0d9f89bebba0a7d88f41e15177d56aaa8894cfd2
danichao <danichao@gapp.nthu.edu.tw>
2025-08-27 06:48:12 +0200

    add useful comments

--- host_activity_datelist_incremental_generation_query.sql
+++ host_activity_datelist_incremental_generation_query.sql
@@ -1,10 +1,30 @@
+/*
+ * Incremental query for hosts_cumulated table
+ *
+ * Purpose:
+ *   Update hosts_cumulated table by generating 
+ *   and extendinghost_activity_datelist column 
+ *   day by day in hosts_cumulated table.
+ * 
+ *   host_activity_datelist column tracks 
+ *   dates a host is experiencing any activity.
+ * 
+ * Tables:
+ *   - hosts_cumulated (target table)
+ *   - events table (source table)
+ */
+
+
 INSERT INTO hosts_cumulated
+
+-- Snapshot at a day of hosts_cumulated table
 WITH yesterday AS (
     SELECT *
     FROM hosts_cumulated
     WHERE cur_date = DATE('2023-01-03')
 ),
 
+-- Extract host experiencing activity on next day
 today AS (
     SELECT
         host,
@@ -14,6 +34,13 @@ today AS (
     GROUP BY host, cur_date
 )
 
+/*
+ * Final SELECT to INSERT INTO hosts_cumulated
+ *   - hosts_cumulated:
+ *       * Create array with next day if no hosts_cumulated record
+ *       * Inherit from hosts_cumulated record if no activity next day
+ *       * Append next day if there are both activity and hosts_cumulated record
+ */
 SELECT
     COALESCE(y.host, t.host) AS host,
     CASE 
